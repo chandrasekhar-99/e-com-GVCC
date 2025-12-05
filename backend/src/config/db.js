e66444e2bc0518850eb61954a571579c67c4fe77
chandrasekhar <chandrasekharsathavalli99@gmail.com>
2025-12-05 23:02:31 +0530

    deployment errors fixed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,22 +5,22 @@ const path = require('path');
 
 
 
-const isRender = process.env.RENDER === 'true';
+const isRender = process.env.RENDER=== 'true';
 
-const dbDir = isRender ? process.env.PROD_DB_PATH || '/var/data': process.env.LOCAL_DB_PATH || path.resolve(__dirname, '../../db'); 
+const dbDir = isRender ? '/tmp': path.resolve(__dirname, '../../db'); 
 const dbPath = path.join(dbDir, 'database.sqlite');
 
 
-if (!fs.existsSync(dbDir)) {
-  fs.mkdirSync(dbDir, { recursive: true });
+if (!isRender) {
+  if (!fs.existsSync(dbDir)) {
+    fs.mkdirSync(dbDir, { recursive: true });
+  }
 }
 
 if (!fs.existsSync(dbPath)) {
-  fs.writeFileSync(dbPath, '');
-  console.log('SQLite database created at:', dbPath);
+  fs.writeFileSync(dbPath, "");
+  console.log("SQLite DB created at:", dbPath);
 }
-
-
 const db = new Database(dbPath);
 
 module.exports = db;
